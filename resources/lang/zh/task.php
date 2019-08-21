8e5faccafce84590ce08a201bc6223b5545460b0
wzj <wzj@20b4a918-80b7-da47-a40b-deef5b90e84c>
2019-08-21 08:12:07 +0000

    git-svn-id: https://39.108.144.9:8443/svn/lingpao@51 20b4a918-80b7-da47-a40b-deef5b90e84c

--- resources/lang/zh/task.php
+++ resources/lang/zh/task.php
@@ -4,5 +4,21 @@ return [
     'take_order' => [
         'be_accepted' => '您好，您发布的代拿任务已被接',
         'be_finished' => '您好，接单人已完成你交付的任务，赶紧去看看。如果满意，请给任务结算吧。',
+        'order_status' => [
+            'unpaid' => '待支付',
+            'new' => '可接单',
+            'cancel'=> '取消',
+            'accepted' => '已接单',
+            'finish' => '已完成',
+            'completed' => '已结算',
+        ],
+        'order_cancel_status' => [
+            'user_apply_cancel' => '用户申请取消',
+            'deliverer_apply_cancel' => '骑手申请取消',
+            'user_agree_cancel' => '用户同意取消',
+            'refunding' => '退款中',
+            'refunded' => '已退款',
+        ],
     ],
+
 ];
