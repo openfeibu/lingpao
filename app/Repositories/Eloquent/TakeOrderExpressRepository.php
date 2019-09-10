048366cac095bf51e6ebc9011af31170f882e5af
wzj <wzj@20b4a918-80b7-da47-a40b-deef5b90e84c>
2019-09-10 08:31:37 +0000

    git-svn-id: https://39.108.144.9:8443/svn/lingpao@185 20b4a918-80b7-da47-a40b-deef5b90e84c

--- app/Repositories/Eloquent/TakeOrderExpressRepository.php
+++ app/Repositories/Eloquent/TakeOrderExpressRepository.php
@@ -36,7 +36,7 @@ class TakeOrderExpressRepository extends BaseRepository implements TakeOrderExpr
         $take_order = app(TakeOrderRepository::class)->find($take_order_id,['user_id','deliverer_id']);
 
         $user_id = User::isLogin();
-        if(!$user_id || ($user_id != $take_order->user_id && $$user_id != $take_order->deliverer_id))
+        if(!$user_id || ($user_id != $take_order->user_id && $user_id != $take_order->deliverer_id))
         {
             $expresses = $this->where('take_order_id',$take_order_id)
                 ->orderBy('id','asc')
