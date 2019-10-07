591d9c41f45cacffb516d9c71e3543f5634c4fe0
wzj <wzj@20b4a918-80b7-da47-a40b-deef5b90e84c>
2019-10-07 01:36:48 +0000

    git-svn-id: https://39.108.144.9:8443/svn/lingpao@210 20b4a918-80b7-da47-a40b-deef5b90e84c

--- app/Http/Controllers/Api/UserCouponController.php
+++ app/Http/Controllers/Api/UserCouponController.php
@@ -28,11 +28,11 @@ class UserCouponController extends BaseController
     public function getUserCoupons(Request $request)
     {
         $user_balance_coupon_data = [];
-        $user_balance_coupon = app(UserBalanceCoupon::class)
-            ->select(DB::raw("uac.id,user_balance_coupons.id as user_balance_coupon_id,user_balance_coupons.user_id, user_balance_coupons.price, user_balance_coupons.balance,'balance_coupon' as type"))
+        $user_balance_coupon = UserBalanceCoupon::select(DB::raw("uac.id,user_balance_coupons.id as user_balance_coupon_id,user_balance_coupons.user_id, user_balance_coupons.price, user_balance_coupons.balance,'balance_coupon' as type,user_balance_coupons.created_at"))
             ->join('user_all_coupons as uac','uac.objective_id','=','user_balance_coupons.id')
             ->where('user_balance_coupons.user_id',$this->user->id)
-            ->where('uac.objective_model','UserBalanceCoupon')
+            ->whereRaw("date_add(`user_balance_coupons`.`created_at`, interval 1 MONTH)  >= "."'".date('Y-m-d')."'")
+            ->where('uac.objective_model','=','UserBalanceCoupon')
             ->first();
         if($user_balance_coupon)
         {
--- app/Repositories/Eloquent/UserAllCouponRepository.php
+++ app/Repositories/Eloquent/UserAllCouponRepository.php
@@ -34,13 +34,13 @@ class UserAllCouponRepository extends BaseRepository implements UserAllCouponRep
                 ];
                 break;
             case 'UserBalanceCoupon':
-                $user_balance_coupon = app(UserBalanceCouponRepository::class)->where(['user_id' => $user_id,'id' => $coupon->objective_id])->first();
+                $user_balance_coupon = UserBalanceCoupon::where(['user_id' => $user_id,'id' => $coupon->objective_id])->whereRaw("date_add(`created_at`, interval 1 MONTH)  >= "."'".date('Y-m-d')."'")->first();
                 if(!$user_balance_coupon)
                 {
                     throw new OutputServerMessageException('优惠券不存在');
                 }
                 if($user_balance_coupon->balance <= 0)
-                {
+                {   
                     throw new OutputServerMessageException('优惠券余额已为0');
                 }
                 $price = min([rid_two($total_price * floatval(setting('balance_coupon_rate'))),$user_balance_coupon->balance]);
