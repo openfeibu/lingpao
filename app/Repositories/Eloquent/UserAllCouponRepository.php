b2b723598a896fd5d7efa19bd69af3818c437664
wzj <wzj@20b4a918-80b7-da47-a40b-deef5b90e84c>
2019-10-08 02:44:45 +0000

    git-svn-id: https://39.108.144.9:8443/svn/lingpao@213 20b4a918-80b7-da47-a40b-deef5b90e84c

--- app/Repositories/Eloquent/UserAllCouponRepository.php
+++ app/Repositories/Eloquent/UserAllCouponRepository.php
@@ -7,6 +7,7 @@ use App\Repositories\Eloquent\UserAllCouponRepositoryInterface;
 use App\Repositories\Eloquent\BaseRepository;
 use App\Models\User;
 use App\Models\UserAllCoupon;
+use App\Models\UserBalanceCoupon;
 
 class UserAllCouponRepository extends BaseRepository implements UserAllCouponRepositoryInterface
 {
